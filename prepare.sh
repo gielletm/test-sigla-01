4b9b18365e8fee1f9db57a73f2dec630abb1af28
gielletm <42549089+gielletm@users.noreply.github.com>
2022-03-23 10:43:40 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 cd ..
 echo "Cloning SIGLA repo ..."
-git clone https://github.com/consiglionazionaledellericerche/sigla-ng.git
+git clone https://github.com/consiglionazionaledellericerche/sigla-main.git
 echo "Moving cloudbuild.yaml in target folder ..."
-mv ./test-sigla-01/cloudbuild.yaml ./sigla-ng/
+mv ./cloud-google-sigla/cloudbuild.yaml ./sigla-main/
 echo "Changing current folder ..."
-cd sigla-ng
+cd sigla-main
